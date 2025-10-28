0653a7c2cad0c7f999e7e996b9cb2619106a02bf
kalil0321 <kalil.bouzigues@gmail.com>
2025-10-28 01:04:16 +0100

    Add Pydantic models for Greenhouse job data structure, defining attributes such as job details, compliance items, locations, metadata, departments, and offices for structured API response handling.

---
+++ models/gh.py
@@ -0,0 +1,48 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional, Any, Union
+
+class DataComplianceItem(BaseModel):
+    type: Optional[str]
+    requires_consent: Optional[bool]
+    requires_processing_consent: Optional[bool]
+    requires_retention_consent: Optional[bool]
+    retention_period: Optional[Any]
+    demographic_data_consent_applies: Optional[bool]
+
+class Location(BaseModel):
+    name: Optional[str]
+
+class MetadataItem(BaseModel):
+    id: Optional[int]
+    name: Optional[str]
+    value: Optional[List[str]]
+    value_type: Optional[str]
+
+class Department(BaseModel):
+    id: Optional[int]
+    name: Optional[str]
+    child_ids: Optional[List[int]]
+    parent_id: Optional[Union[int, None]]
+
+class Office(BaseModel):
+    id: Optional[int]
+    name: Optional[str]
+    location: Optional[str]
+    child_ids: Optional[List[int]]
+    parent_id: Optional[Union[int, None]]
+
+class GreenhouseJob(BaseModel):
+    absolute_url: Optional[str]
+    data_compliance: Optional[List[DataComplianceItem]]
+    internal_job_id: Optional[int]
+    location: Optional[Location]
+    metadata: Optional[List[MetadataItem]]
+    id: Optional[int]
+    updated_at: Optional[str]
+    requisition_id: Optional[str]
+    title: Optional[str]
+    company_name: Optional[str]
+    first_published: Optional[str]
+    content: Optional[str]
+    departments: Optional[List[Department]]
+    offices: Optional[List[Office]]
